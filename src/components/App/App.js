4edd906fc005d6a88ceeb15eedcd56090cae153b
seyelich <robot11148@gmail.com>
2023-10-20 00:52:10 +0700

    feat: add routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router';
 import './App.css';
+import NotFound from '../../pages/NotFound/NotFound';
 import Header from '../Header/Header';
 
 function App() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route element={<p>Здесь главная стр</p>} path="/" />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
