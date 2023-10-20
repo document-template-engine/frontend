8b1a563e5e39365236886d08fd874a439ea8d565
Katerina <108937960+Kale0n@users.noreply.github.com>
2023-10-20 22:33:02 +0300

    Feature/navbar (#36)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import { Route, Routes } from 'react-router';
 import './App.css';
 import NotFound from '../../pages/NotFound/NotFound';
+import { useState } from 'react';
+import Header from '../Header/Header';
+import Navbar from '../Navbar/Navbar';
 
 function App() {
+	const [loggedIn, setLoggedIn] = useState(true);
+
 	return (
-		<Routes>
-			<Route element={<p>Здесь главная стр</p>} path="/" />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<>
+			<Header isLoggedIn={loggedIn} />
+			<Navbar isLoggedIn={loggedIn} isTemplatePage />
+      <Routes>
+        <Route element={<p>Здесь главная стр</p>} path="/" />
+        <Route path="*" element={<NotFound />} />
+		  </Routes>
+		</>
 	);
 }
 
