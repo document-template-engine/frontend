edb15374d44f35d503170a2c027b0224d1f35562
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-10-23 13:43:49 +0300

    feature/templates feature/templates-routing feature/pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom'; // Импортируйте Outlet для вложенных маршрутов
 import Header from '../Header/Header';
-import Templates from '../pages/Templates';
+import Templates from '../../pages/Templates';
 import NotFound from '../NotFound/NotFound';
 import Navbar from '../Navbar/Navbar';
-import ApplicationToKindergarten from '../Templates/ApplicationToKindergarten';
+import ApplicationToKindergarten from '../../pages/Templates/ApplicationToKindergarten';
 
 function App() {
 	const [loggedIn, setLoggedIn] = useState(true);
