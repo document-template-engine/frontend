a7d217344023726279bd7239dc2c470aae3f4979
Yelizaveta Chepurina <robot11148@gmail.com>
2023-10-23 21:02:50 +0300

    feat: add auth forms (#50)
    * feat: add auth forms

---
+++ src/pages/AuthPages/ChangePassword.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthForm from '../../components/AuthForm/AuthTemplate';
+import Modal from '../../components/Modal/Modal';
+import Input from '../../stories/Input/Input';
+import styles from './index.module.scss';
+import Button from '../../stories/Button/Button';
+
+export default function ChangePasswordPage() {
+	const [visible, setVisible] = useState(true);
+	const navigate = useNavigate();
+
+	const handleClose = () => {
+		setVisible(false);
+		navigate('/');
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		navigate('/');
+	};
+
+	return (
+		visible && (
+			<Modal hasOverlay handleClose={handleClose}>
+				<AuthForm title="Смена пароля">
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<Input type="password" name="password" label="Пароль" />
+						<Button type="submit" text="Сохранить" />
+					</form>
+				</AuthForm>
+			</Modal>
+		)
+	);
+}
---
+++ src/pages/AuthPages/LogupPage.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import Modal from '../../components/Modal/Modal';
+import AuthForm from '../../components/AuthForm/AuthTemplate';
+import Input from '../../stories/Input/Input';
+import checkmark from '../../images/checkmark.svg';
+import styles from './index.module.scss';
+import Button from '../../stories/Button/Button';
+
+export default function LogupPage() {
+	const [visible, setVisible] = useState(true);
+	const [checked, setChecked] = useState(false);
+	const navigate = useNavigate();
+
+	const handleClose = () => {
+		setVisible(false);
+		navigate('/');
+	};
+
+	const toggleBtn = () => {
+		setChecked(!checked);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		navigate('/check-account');
+	};
+
+	return (
+		visible && (
+			<Modal hasOverlay handleClose={handleClose}>
+				<AuthForm title="Создание аккаунта">
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<Input type="text" name="email" label="Электронная почта" />
+						<Input
+							type="password"
+							name="pw"
+							label="Пароль"
+							placeholder="dsads"
+						/>
+						<div className={styles.checkboxContainer}>
+							<button
+								className={`${styles.checkbox} ${
+									checked && styles.checkboxChecked
+								}`}
+								type="button"
+								aria-label="savePw"
+								onClick={toggleBtn}
+							>
+								{checked && <img src={checkmark} alt="checkmark" />}
+							</button>
+							<p style={{ margin: 0 }}>
+								Я согласен &nbsp;
+								<Link to={{ pathname: '/' }} className={styles.link}>
+									с политикой конфиденциальности
+								</Link>
+							</p>
+						</div>
+						<Button type="submit" text="Продолжить" />
+						<p className={styles.orPar}>
+							<span>или</span>
+						</p>
+						<Link to={{ pathname: '/signin' }} className={styles.largeLink}>
+							Вход
+						</Link>
+					</form>
+				</AuthForm>
+			</Modal>
+		)
+	);
+}
