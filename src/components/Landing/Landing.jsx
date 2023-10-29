edbc48c13990324b158e44ba0e35e78741fad4a4
Katerina <108937960+Kale0n@users.noreply.github.com>
2023-10-29 16:10:14 +0300

    landing fixed (#75)

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,6 +2,7 @@ import About from './About/About';
 import HowItWorks from './HowItWorks/HowItWorks';
 import Advantages from './Advantages/Advantages';
 import Footer from './Footer/Footer';
+import './Landing.sass';
 
 export default function Landing() {
 	return (
