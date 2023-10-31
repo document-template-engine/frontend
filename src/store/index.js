5fe114db8ba42b48b63b63c8f2ec96eb8fd8f108
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-10-31 19:27:09 +0300

    hotfix/mw-fix

--- src/store/index.js
+++ src/store/index.js
@@ -4,16 +4,20 @@ import userReducer from './user/userSlice';
 import authReducer from './auth/authSlice';
 import { popupReducer } from './popup/popup.slice';
 import { authApi } from './auth-api/auth.api';
+import { templatesApi } from './templates-api/templates.api';
 
 export const store = configureStore({
 	reducer: {
 		[authApi.reducerPath]: authApi.reducer,
+		[templatesApi.reducerPath]: templatesApi.reducer,
 		user: userReducer,
 		auth: authReducer,
 		popup: popupReducer,
 	},
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(authApi.middleware),
+		getDefaultMiddleware()
+			.concat(authApi.middleware)
+			.concat(templatesApi.middleware),
 });
 
 setupListeners(store.dispatch);
