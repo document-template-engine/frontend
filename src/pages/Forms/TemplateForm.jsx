3e5543b2118a8361d12e84145e4a67d2cefed389
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-11-02 23:53:47 +0300

    fix/templates почистил комментарии в темплейтПейдж

--- src/pages/Forms/TemplateForm.jsx
+++ src/pages/Forms/TemplateForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Form.module.sass';
 import CustomInput from '../../components/UI/CustomInput';
-import {largeSize} from '../../utils/constants';
+import {largeSize} from '../../utils/constants'; // eslint-disable-next-line react/prop-types
 
 // eslint-disable-next-line react/prop-types
 const TemplateForm = ({ data }) => {
@@ -32,93 +32,6 @@ const TemplateForm = ({ data }) => {
 					</div>
 				</li>
 			))}
-
-			{/* <li className={styles.item}> */}
-			{/*	<h2 className={styles.title}>Данные о детском саде</h2> */}
-			{/*	<div className={styles.wrapper}> */}
-			{/*		<CustomInput */}
-			{/*			width={midSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="Номер или название детского сада" */}
-			{/*		/> */}
-			{/*	</div> */}
-			{/* </li> */}
-			{/* <li className={styles.itemSuccess}> */}
-			{/*	<h2 className={styles.title}>Адресат</h2> */}
-			{/*	<div className={styles.wrapper}> */}
-			{/*		<CustomInput */}
-			{/*			width={largeSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="ФИО заведующего в дательном падеже" */}
-			{/*			notation="Иванову Алексею Дмитриевичу" */}
-			{/*		/> */}
-			{/*	</div> */}
-			{/* </li> */}
-			{/* <li className={styles.item}> */}
-			{/*	<h2 className={styles.title}>Отправитель</h2> */}
-			{/*	<div className={styles.wrapper}> */}
-			{/*		<CustomInput */}
-			{/*			width={midSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="ФИО родителя/законного представителя в родительном падеже" */}
-			{/*			notation="Иванова Алексея Дмитриевича" */}
-			{/*		/> */}
-			{/*		<CustomInput */}
-			{/*			width={midSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="Почтовый адрес" */}
-			{/*			notation="ул. Некрасова, дом 12 к1, кв.41" */}
-			{/*		/> */}
-			{/*	</div> */}
-			{/* </li> */}
-			{/* <li className={styles.item}> */}
-			{/*	<h2 className={styles.title}>Ребенок</h2> */}
-			{/*	<div className={styles.wrapper}> */}
-			{/*		<CustomInput */}
-			{/*			width={largeSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="ФИО ребенка в творительном падеже" */}
-			{/*			notation="Иванову Алексею Дмитриевичу" */}
-			{/*		/> */}
-			{/*		<CustomInput */}
-			{/*			width={midSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			text="Номер или название группы в детском саду" */}
-			{/*		/> */}
-			{/*	</div> */}
-			{/* </li> */}
-			{/* <li className={styles.item}> */}
-			{/*	<h2 className={styles.title}>Даты</h2> */}
-			{/*	<div className={styles.wrapper}> */}
-			{/*		<CustomInput */}
-			{/*			width={smallSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			notation="ДД.ММ.ГГГГ" */}
-			{/*			text="Дата начала отпуска" */}
-			{/*		/> */}
-			{/*		<CustomInput */}
-			{/*			width={smallSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			notation="ДД.ММ.ГГГГ" */}
-			{/*			text="Дата окончания отпуска" */}
-			{/*		/> */}
-			{/*		<CustomInput */}
-			{/*			width={smallSize} */}
-			{/*			form={form} */}
-			{/*			type="text" */}
-			{/*			notation="ДД.ММ.ГГГГ" */}
-			{/*			text="Дата написания заявления" */}
-			{/*		/> */}
-			{/*	</div> */}
-			{/* </li> */}
 		</ol>
 	);
 };
