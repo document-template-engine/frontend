7f275ce9f2063574ed453caac3ecc917d062d58f
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-11-06 21:58:32 +0300

    templates/ Навел порядок с темплейтами, добавил комментарии

--- src/components/Templates/TemplateList.jsx
+++ src/components/Templates/TemplateList.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './TemplateList.module.sass';
-import Navbar from '../Navbar/Navbar';
-import Header from '../Header/Header';
 import TemplateItem from './TemplateItem';
 import {useGetTemplatesQuery} from '../../store/templates-api/templates.api';
 
@@ -11,25 +9,21 @@ const TemplateList = ({ title }) => {
 	const { data } = useGetTemplatesQuery();
 
 	return (
-		<>
-			<Header />
-			<Navbar isTemplatePage />
-			<div className={styles.templates}>
-				<h1 className={styles.title}>{title}</h1>
-				<ul className={styles.list}>
-					{data &&
-						data.map((item) => (
-							<TemplateItem
-								title={item.name}
-								link={item.id.toString()}
-								isFav={item.is_favorited}
-								img={item.img}
-								key={item.id}
-							/>
-						))}
-				</ul>
-			</div>
-		</>
+		<div className={styles.templates}>
+			<h1 className={styles.title}>{title}</h1>
+			<ul className={styles.list}>
+				{data &&
+					data.map((item) => (
+						<TemplateItem
+							title={item.name}
+							link={item.id.toString()}
+							isFav={item.is_favorited}
+							img={item.img}
+							key={item.id}
+						/>
+					))}
+			</ul>
+		</div>
 	);
 };
 
--- src/pages/TemplatesMain.jsx
+++ src/pages/TemplatesMain.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import TemplateList from '../components/Templates/TemplateList';
+import Header from '../components/Header/Header';
+import Navbar from '../components/Navbar/Navbar';
 
 const TemplatesMain = () => {
 	const location = useLocation();
 	const currentPath = location.pathname;
 	// Если страничка главная - то он показывает список
 	if (currentPath === '/templates') {
-		return <TemplateList title="Шаблоны" />;
+		return (
+			<>
+				<Header />
+				<Navbar isTemplatePage />
+				<TemplateList title="Шаблоны" />
+			</>
+		);
 	}
 	// Если не главная - показывает форму соответствующей странички
 	return <Outlet />;
