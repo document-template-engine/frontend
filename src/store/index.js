cb2f1ed0afd1b007fd57cf7241bbe71938ce72df
Pavel Shershnev <pavelsersnev@gmail.com>
2023-11-09 22:42:50 +1200

    adaptive

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import authReducer from './auth/authSlice';
 import { popupReducer } from './popup/popup.slice';
 import { authApi } from './auth-api/auth.api';
 import { templatesApi } from './templates-api/templates.api';
+import windowWidth from './window-width/windowWidthSlice';
 
 export const store = configureStore({
 	reducer: {
@@ -13,6 +14,7 @@ export const store = configureStore({
 		user: userReducer,
 		auth: authReducer,
 		popup: popupReducer,
+		windowWidth,
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware()
