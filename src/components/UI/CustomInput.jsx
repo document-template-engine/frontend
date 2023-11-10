a23b09fbb359802fc96cfcc3ebde25d1d142258b
Павел Шершнёв <pavelsersnev@gmail.com>
2023-11-10 22:12:41 +1200

    reverse

--- src/components/UI/CustomInput.jsx
+++ src/components/UI/CustomInput.jsx
@@ -1,29 +1,25 @@
 import React, { useState } from 'react';
 import styles from './CustomInput.module.sass';
-import { useValidation } from '../../hooks/useValidation';
 
 // eslint-disable-next-line react/prop-types
-const CustomInput = ({ form, width = 'L', type, text, notation, reg, id }) => {
-	const { handleChange, errors } = useValidation();
+const CustomInput = ({ form, width = 'L', type, text, notation }) => {
+	const [values, setValues] = useState();
 
 	return (
-		<form onChange={handleChange} action="#" className={styles.graph}>
+		<div className={styles.graph}>
 			<label htmlFor={form} className={styles.label}>
 				<p className={styles.title} style={{ width }}>
 					{text}
 				</p>
 				<input
-					name={id}
 					type={type}
 					placeholder=""
 					className={styles.input}
 					style={{ width }}
-					pattern={reg ?? ''}
 				/>
-				<span className={styles.error}>{reg && errors[id]}</span>
 			</label>
 			{notation && <p className={styles.notation}>{notation}</p>}
-		</form>
+		</div>
 	);
 };
 
