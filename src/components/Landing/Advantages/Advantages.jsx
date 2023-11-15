66aebe583f2c2346a4030760387203f35de6e318
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-11-16 02:03:12 +0300

    правки

--- src/components/Landing/Advantages/Advantages.jsx
+++ src/components/Landing/Advantages/Advantages.jsx
@@ -2,8 +2,6 @@ import './Advantages.sass';
 import interfaceImg from '../../../images/lan-interface.jpg';
 import templateImg from '../../../images/lan-template.jpg';
 import docImg from '../../../images/lan-doc.jpg';
-import octopusImg from '../../../images/lan-octopus.svg';
-import snakeImg from '../../../images/lan-snake.svg';
 import LinkToStart from '../Link/LinkToStart';
 
 export default function Advantages() {
@@ -12,67 +10,59 @@ export default function Advantages() {
 			<h2 className="advantages__title">
 				Используйте преимущества шаблонизатора
 			</h2>
-			<div className="advantages__container">
-				<div className="advantages__list-container">
-					<ul className="advantages__list">
-						<li className="advantages__item-container advantages__item-container_1">
-							<p className="advantages__item-text">
-								Удобный
-								<br />
-								интерфейс
-							</p>
-						</li>
-						<li className="advantages__item-container advantages__item-container_2">
-							<p className="advantages__item-text">
-								Категории
-								<br />
-								Шаблонов
-							</p>
-						</li>
-						<li className="advantages__item-container advantages__item-container_3">
-							<p className="advantages__item-text">
-								Быстро
-								<br />и удобно
-							</p>
-						</li>
-						<li className="advantages__item-container advantages__item-container_4">
-							<p className="advantages__item-text">
-								Пригодно
-								<br />
-								для печати
-							</p>
-						</li>
-					</ul>
-					<img
-						className="advantages__lines-1"
-						src={octopusImg}
-						alt="Пунктирные линии"
-					/>
-					<img
-						className="advantages__lines-2"
-						src={snakeImg}
-						alt="Пунктирные линии"
-					/>
+			<div className="advantages__content">
+				<div className="advantages__container">
+					<div className="advantages__list-container">
+						<ul className="advantages__list">
+							<li className="advantages__item-container advantages__item-container_1">
+								<p className="advantages__item-text">
+									Удобный
+									<br />
+									интерфейс
+								</p>
+							</li>
+							<li className="advantages__item-container advantages__item-container_2">
+								<p className="advantages__item-text">
+									Категории
+									<br />
+									Шаблонов
+								</p>
+							</li>
+							<li className="advantages__item-container advantages__item-container_3">
+								<p className="advantages__item-text">
+									Быстро
+									<br />и удобно
+								</p>
+							</li>
+							<li className="advantages__item-container advantages__item-container_4">
+								<p className="advantages__item-text">
+									Пригодно
+									<br />
+									для печати
+								</p>
+							</li>
+						</ul>
+					</div>
+					<div className="advantages__illustration-container">
+						<img
+							className="advantages__illustration advantages__illustration_1"
+							src={interfaceImg}
+							alt="Интерфейс приложения"
+						/>
+						<img
+							className="advantages__illustration advantages__illustration_2"
+							src={templateImg}
+							alt="Шаблон"
+						/>
+						<img
+							className="advantages__illustration advantages__illustration_3"
+							src={docImg}
+							alt="Готовый документ"
+						/>
+					</div>
 				</div>
-				<div className="advantages__illustration-container">
-					<img
-						className="advantages__illustration advantages__illustration_1"
-						src={interfaceImg}
-						alt="Интерфейс приложения"
-					/>
-					<img
-						className="advantages__illustration advantages__illustration_2"
-						src={templateImg}
-						alt="Шаблон"
-					/>
-					<img
-						className="advantages__illustration advantages__illustration_3"
-						src={docImg}
-						alt="Готовый документ"
-					/>
-				</div>
+				<LinkToStart />
 			</div>
-			<LinkToStart />
 		</section>
 	);
 }
