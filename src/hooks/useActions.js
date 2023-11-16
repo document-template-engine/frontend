fd5fad263ece7551fa97ea5857d6df87552ea312
Павел Шершнёв <pavelsersnev@gmail.com>
2023-11-16 19:55:24 +1200

    fix/conflict

--- src/hooks/useActions.js
+++ src/hooks/useActions.js
@@ -3,11 +3,13 @@ import { bindActionCreators } from '@reduxjs/toolkit';
 import { popupActions } from '../store/popup/popup.slice';
 import { emailActions } from '../store/auth/regSlice';
 import { formActions } from '../store/form/form.slice';
+import { searchQueryActions } from '../store/search-query/search-query.slice';
 
 const actions = {
 	...popupActions,
 	...emailActions,
 	...formActions,
+	...searchQueryActions,
 };
 
 export const useActions = () => {
