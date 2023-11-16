e16b53096244e3f0c89b6f216b56b36b8df2eb5a
Павел Шершнёв <pavelsersnev@gmail.com>
2023-11-16 13:36:08 +1200

    fix-navbar/add navpanel

--- src/components/Landing/Advantages/Advantages.jsx
+++ src/components/Landing/Advantages/Advantages.jsx
@@ -7,35 +7,72 @@ import snakeImg from '../../../images/lan-snake.svg';
 import LinkToStart from '../Link/LinkToStart';
 
 export default function Advantages() {
-    return (
-        <section className='advantages'>
-            <h2 className='advantages__title'>Используйте преимущества шаблонизатора</h2>
-            <div className='advantages__container'>
-                <div className='advantages__list-container'>
-                    <ul className='advantages__list'>
-                        <li className='advantages__item-container advantages__item-container_1'>
-                            <p className='advantages__item-text'>Удобный<br />интерфейс</p>
-                        </li>
-                        <li className='advantages__item-container advantages__item-container_2'>
-                            <p className='advantages__item-text'>Категории<br />Шаблонов</p>
-                        </li>
-                        <li className='advantages__item-container advantages__item-container_3'>
-                            <p className='advantages__item-text'>Быстро<br />и удобно</p>
-                        </li>
-                        <li className='advantages__item-container advantages__item-container_4'>
-                            <p className='advantages__item-text'>Пригодно<br />для печати</p>
-                        </li>
-                    </ul>
-                    <img className='advantages__lines-1' src={octopusImg} alt='Пунктирные линии'/>
-                    <img className='advantages__lines-2' src={snakeImg} alt='Пунктирные линии'/>
-                </div>
-                <div className='advantages__illustration-container'>
-                    <img className='advantages__illustration advantages__illustration_1' src={interfaceImg} alt='Интерфейс приложения'/>
-                    <img className='advantages__illustration advantages__illustration_2' src={templateImg} alt='Шаблон'/>
-                    <img className='advantages__illustration advantages__illustration_3' src={docImg} alt='Готовый документ'/>
-                </div>
-            </div>
-            <LinkToStart />  
-        </section>
-    );
-};
+	return (
+		<section className="advantages">
+			<h2 className="advantages__title">
+				Используйте преимущества шаблонизатора
+			</h2>
+			<div className="advantages__container">
+				<div className="advantages__list-container">
+					<ul className="advantages__list">
+						<li className="advantages__item-container advantages__item-container_1">
+							<p className="advantages__item-text">
+								Удобный
+								<br />
+								интерфейс
+							</p>
+						</li>
+						<li className="advantages__item-container advantages__item-container_2">
+							<p className="advantages__item-text">
+								Категории
+								<br />
+								Шаблонов
+							</p>
+						</li>
+						<li className="advantages__item-container advantages__item-container_3">
+							<p className="advantages__item-text">
+								Быстро
+								<br />и удобно
+							</p>
+						</li>
+						<li className="advantages__item-container advantages__item-container_4">
+							<p className="advantages__item-text">
+								Пригодно
+								<br />
+								для печати
+							</p>
+						</li>
+					</ul>
+					<img
+						className="advantages__lines-1"
+						src={octopusImg}
+						alt="Пунктирные линии"
+					/>
+					<img
+						className="advantages__lines-2"
+						src={snakeImg}
+						alt="Пунктирные линии"
+					/>
+				</div>
+				<div className="advantages__illustration-container">
+					<img
+						className="advantages__illustration advantages__illustration_1"
+						src={interfaceImg}
+						alt="Интерфейс приложения"
+					/>
+					<img
+						className="advantages__illustration advantages__illustration_2"
+						src={templateImg}
+						alt="Шаблон"
+					/>
+					<img
+						className="advantages__illustration advantages__illustration_3"
+						src={docImg}
+						alt="Готовый документ"
+					/>
+				</div>
+			</div>
+			<LinkToStart />
+		</section>
+	);
+}
