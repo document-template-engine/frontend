a144d53c482cd7c3d4d56b09b5ce33dadd2dc2ed
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-11-19 16:45:14 +0300

    правки

--- src/components/UI/PreloaderWithOverlay/PreloaderWithOverlay.jsx
+++ src/components/UI/PreloaderWithOverlay/PreloaderWithOverlay.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import styles from './PreloaderWithOverlay.sass';
+import styles from './PreloaderWithOverlay.module.sass';
 import Preloader from '../Preloader/Preloader';
 
-const PreloaderWithOverlay = () => {
-	console.log(styles);
-
-	return (
-		<div className={styles.overlay}>
-			<div className={styles.mainWrapper}>
-				<Preloader style={{ maxHeight: 'fit-content' }} />
-				<div className={styles.textWrapper}>
-					<h1 className={styles.title}>Секунду</h1>
-					<p className={styles.subtitle}>Формируем документ</p>
-				</div>
+const PreloaderWithOverlay = () => (
+	<div className={styles.overlay}>
+		<div className={styles.mainWrapper}>
+			<Preloader />
+			<div className={styles.textWrapper}>
+				<h1 className={styles.title}>Секунду</h1>
+				<p className={styles.subtitle}>Формируем документ</p>
 			</div>
 		</div>
-	);
-};
+	</div>
+);
 
 export default PreloaderWithOverlay;
