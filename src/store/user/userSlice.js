024b5f280944b2c63bbcba5e1528ef5ad8dd0713
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-11-20 03:26:31 +0300

    user slice init

--- src/store/user/userSlice.js
+++ src/store/user/userSlice.js
@@ -12,6 +12,25 @@ export const userSlice = createSlice({
 		logout(state, action) {
 			state = initialState;
 		},
+		verifyToken(state) {
+			return fetch('https://documents-template.site/api/users/me', {
+				headers: {
+					Authorization: `Token ${localStorage.getItem('token')}`,
+				},
+			})
+				.then((res) => {
+					if (res.ok) {
+						return res.json();
+					}
+					return Promise.reject(res.status);
+				})
+				.then((res) => {
+					state.isLoggedIn = true;
+					state.userId = res.id;
+					state.email = res.email;
+				})
+				.catch(console.log);
+		},
 	},
 });
 
