443c5f7d618fd797ff2e44f41d2c3e88aa5208c1
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-11-22 17:30:43 +0300

    исправил ошибку пустых полей

--- src/components/TemplateForm/TemplateForm.jsx
+++ src/components/TemplateForm/TemplateForm.jsx
@@ -23,7 +23,7 @@ import Preloader from '../UI/Preloader/Preloader';
 import { useActions } from '../../hooks/useActions';
 
 export default function TemplateForm() {
-	const { changePdfViewFile, resetForm } = useActions();
+	const { changePdfViewFile, resetForm, setFormData } = useActions();
 
 	const location = useLocation();
 	const currentPath = location.pathname;
@@ -252,11 +252,12 @@ export default function TemplateForm() {
 
 	// хендлер добавления в избранное
 	const saveAsFavouriteHandler = () => {
-		if (!isFavorited) {
-			fetchFavorite(temp.id).then(setIsFavorited(true));
-		} else {
-			deleteFavorite(temp.id).then(setIsFavorited(false));
-		}
+		console.log(formData);
+		// if (!isFavorited) {
+		// 	fetchFavorite(temp.id).then(setIsFavorited(true));
+		// } else {
+		// 	deleteFavorite(temp.id).then(setIsFavorited(false));
+		// }
 	};
 
 	const watchPDFHandler = () => {
@@ -289,6 +290,18 @@ export default function TemplateForm() {
 		}
 		return null;
 	};
+	useEffect(() => {
+		setFormData([]);
+		const result = [];
+
+		temp?.grouped_fields?.forEach((item) =>
+			item.fields.forEach(
+				(field) =>
+					field.value && result.push({ field: field.id, value: field.value })
+			)
+		);
+		setFormData([...formData, ...result]);
+	}, [temp]);
 
 	useEffect(() => {
 		if (currentPath === `/drafts/${id}` || currentPath === `/docs/${id}`) {
