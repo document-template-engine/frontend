677027fe9f16b449df4567dd56ae6e18b1031f40
Davydenkov Ivan <118854180+IvanDavydenkov@users.noreply.github.com>
2023-11-24 22:50:27 +0300

    [правки] определил поведение ActionBar

--- src/components/TemplateForm/DocsForm.jsx
+++ src/components/TemplateForm/DocsForm.jsx
@@ -132,7 +132,7 @@ export const DocsForm = () => {
 					downloadPDFHandler={handleDownloadPdf}
 					saveAsDraftHandler={saveAsDraftHandler}
 					watchPDFHandler={watchPDFHandler}
-					idDraft={id}
+					isDocPage
 				/>
 			</form>
 		)
